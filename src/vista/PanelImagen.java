003a562055b719fe77a05ad773664b6c1693a89d
Luisa Marinez <luisa.marinez1@correo.icesi.edu.co>
2021-04-15 14:10:07 -0500

    anadiendo panelimage

---
+++ src/vista/PanelImagen.java
@@ -0,0 +1,13 @@
+package vista;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+import controlador.Movimiento;
+
+
+public class PanelImagen extends JPanel {
+} 
